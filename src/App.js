0a0e53fadd743efae87ea56c5729ca4d7d64355b
Savio Abuga <savioabuga@gmail.com>
2016-11-16 09:22:08 +0300

    setup react-router 4 and created games page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Link, Match } from 'react-router';
+import GamesPage from './GamesPage';
 import logo from './logo.svg';
 import './App.css';
 
@@ -11,8 +13,9 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <Link to="games">Games</Link>
         </p>
+        <Match pattern="/games" component={GamesPage} />
       </div>
     );
   }
